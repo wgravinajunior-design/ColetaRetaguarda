9566988ccb64202bdfb0bc0595cc968c2c54a378
WalterGoUp <151241172+WalterGoUp@users.noreply.github.com>
2026-07-14 08:24:29 -0300

    Corrigir tipos DateTime para String em RotaModel - SQLite compatibility

--- lib/features/rotas/models/rota_model.dart
+++ lib/features/rotas/models/rota_model.dart
@@ -37,15 +37,9 @@ class RotaModel {
       motoristaId: json['ROT_MOTORISTA_ID'] as int?,
       veiculoId: json['ROT_VEICULO_ID'] as int?,
       status: json['ROT_STATUS'] as String? ?? 'A',
-      dataPrevista: json['ROT_DATA_PREVISTA'] != null
-          ? DateTime.tryParse(json['ROT_DATA_PREVISTA'] as String)
-          : null,
-      dataInicio: json['ROT_DATA_INICIO'] != null
-          ? DateTime.tryParse(json['ROT_DATA_INICIO'] as String)
-          : null,
-      dataFim: json['ROT_DATA_FIM'] != null
-          ? DateTime.tryParse(json['ROT_DATA_FIM'] as String)
-          : null,
+      dataPrevista: json['ROT_DATA_PREVISTA'] as String?,
+      dataInicio: json['ROT_DATA_INICIO'] as String?,
+      dataFim: json['ROT_DATA_FIM'] as String?,
       paradas: json['ROT_PARADAS'] as int? ?? 0,
       kmEstimado: (json['ROT_KM_ESTIMADO'] as num?)?.toDouble() ?? 0.0,
       kmRealizado: (json['ROT_KM_REALIZADO'] as num?)?.toDouble() ?? 0.0,
@@ -63,9 +57,9 @@ class RotaModel {
       'ROT_MOTORISTA_ID': motoristaId,
       'ROT_VEICULO_ID': veiculoId,
       'ROT_STATUS': status,
-      'ROT_DATA_PREVISTA': dataPrevista?.toIso8601String(),
-      'ROT_DATA_INICIO': dataInicio?.toIso8601String(),
-      'ROT_DATA_FIM': dataFim?.toIso8601String(),
+      'ROT_DATA_PREVISTA': dataPrevista,
+      'ROT_DATA_INICIO': dataInicio,
+      'ROT_DATA_FIM': dataFim,
       'ROT_PARADAS': paradas,
       'ROT_KM_ESTIMADO': kmEstimado,
       'ROT_KM_REALIZADO': kmRealizado,
